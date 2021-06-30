96aa2b73cdf6c18f67032849a5a11ddbcb66af19
Frojdi Dymylja <frojdi.dymylja@gmail.com>
2021-06-30 08:16:19 +0200

    chore: renaming 2

--- app/app.go
+++ app/app.go
@@ -96,8 +96,8 @@ const appName = "App"
 
 // We pull these out so we can set them with LDFLAGS in the Makefile
 var (
-	NodeDir      = ".wasmd"
-	Bech32Prefix = "wasm"
+	NodeDir      = ".app"
+	Bech32Prefix = "free"
 
 	// ProposalsEnabled If EnabledSpecificProposals is "", and this is "true", then enable all x/wasm proposals.
 	// If EnabledSpecificProposals is "", and this is not "true", then disable all x/wasm proposals.
