aac5e8d6fc7ed9f1c1c9ac3254aa96e952bf1037
Frojdi Dymylja <frojdi.dymylja@gmail.com>
2021-06-30 09:12:01 +0200

    add: cmd files

--- cmd/zoned/main.go
+++ cmd/zoned/main.go
@@ -1,1 +1,22 @@
-package zoned
+package main
+
+import (
+	"os"
+
+	"github.com/CosmWasm/wasmd/app"
+	"github.com/cosmos/cosmos-sdk/server"
+)
+
+func main() {
+	rootCmd, _ := NewRootCmd()
+
+	if err := Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
+	}
+}
