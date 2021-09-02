5aa03b24605661371211e7fd8512f43147c317e9
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2021-09-02 10:55:17 -0400

    add contract B, testing WIP

---
+++ contracts/query-state/src/state.rs
@@ -0,0 +1,14 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::Addr;
+use cw_storage_plus::Item;
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub count: i32,
+    pub owner: Addr,
+    pub counter_contract: Addr,
+}
+
+pub const STATE: Item<State> = Item::new("state");
