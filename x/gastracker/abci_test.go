c195df44647522b85aaab4389bf0b6db591b6a53
Parth Desai <desaiparth08@gmail.com>
2021-11-09 19:59:17 +0400

    refine the test to be more comprehensive

--- x/gastracker/abci_test.go
+++ x/gastracker/abci_test.go
@@ -140,6 +140,14 @@ func TestBlockTracking(t *testing.T) {
 	})
 	require.NoError(t, err, "We should be able to add new contract metadata")
 
+	err = keeper.AddNewContractMetadata(ctx, "4", gstTypes.ContractInstanceMetadata{
+		RewardAddress:   "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt",
+		GasRebateToUser: false,
+		CollectPremium: true,
+		PremiumPercentageCharged: 150,
+	})
+	require.NoError(t, err, "We should be able to add new contract metadata")
+
 	firstTxMaxContractReward := sdk.NewDecCoins(sdk.NewDecCoinFromDec("test", sdk.NewDec(1)), sdk.NewDecCoinFromDec("test1", sdk.NewDec(1).QuoInt64(3)))
 	secondTxMaxContractReward := sdk.NewDecCoins(sdk.NewDecCoinFromDec("test", sdk.NewDec(2)), sdk.NewDecCoinFromDec("test1", sdk.NewDec(1).QuoInt64(2)))
 	// Tracking new block with multiple tx tracking obj
@@ -166,6 +174,12 @@ func TestBlockTracking(t *testing.T) {
 					IsEligibleForReward: true,
 					Operation: gstTypes.ContractOperation_CONTRACT_OPERATION_INSTANTIATION,
 				},
+				{
+					Address: "4",
+					GasConsumed: 1,
+					IsEligibleForReward: false,
+					Operation: gstTypes.ContractOperation_CONTRACT_OPERATION_INSTANTIATION,
+				},
 			},
 		},
 		{
@@ -187,12 +201,13 @@ func TestBlockTracking(t *testing.T) {
 	// Inflation reward cap for a block is hardcoded at 20% of total inflation reward
 	// So, for per block inflation of 765 (76500/100), we need to take 20% of it which is
 	// 153.
-	// Total gas across transaction is 9 (2+4+1+2)
+	// Total gas across block is 10 (2+4+1+2+1)
 	// So, inflation reward for
-	// Contract "1" is: 34 (153 * 2 / 9)
-	// Contract "2" is: 68 (153 * 4 / 9)
-	// Contract "3" is: 17 (153 * 1 / 9)
-	// Contract "2" is: 34 (153 * 2 / 9)
+	// Contract "1" is: 30.6 (153 * 2 / 10)
+	// Contract "2" is: 61.2 (153 * 4 / 10)
+	// Contract "3" is: 15.3 (153 * 1 / 10)
+	// Contract "2" is: 30.6 (153 * 2 / 10)
+	// Contract "4" does not get any reward
 	// All above is in "test" denomination, since that is the denomination minter is minting
 	// Now, coming to gas reward calculations:
 	// For First tx entry:
@@ -204,16 +219,16 @@ func TestBlockTracking(t *testing.T) {
 	//   Gas Used = 2
 	//   "2" Contract's reward is: 2 * (2 / 2) = 2test and 0.5 * (2 / 2) = 0.5test1
 	// Total rewards:
-	// For Contract "1": 34.2test (34 + 0.2) and 0.0666666test1
-	// For Contract "2": 104.4test (68 + 34 + 0.4 + 2) and 0.6333333test1 (0.1333333 + 0.5)
-	// For Contract "3": 17.15test (17 + 0.15) and 0.04999995test1
+	// For Contract "1": 30.8test (30.6 + 0.2) and 0.0666666test1
+	// For Contract "2": 94.2test (61.2 + 30.6 + 0.4 + 2) and 0.6333333test1 (0.1333333 + 0.5)
+	// For Contract "3": 15.45test (15.3 + 0.15) and 0.04999995test1
 	// Reward distribution per address:
-	// (for contract "1") "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt": 34.2test and 0.0666666test1
-	// (for contract "2" and "3") "archway1j08452mqwadp8xu25kn9rleyl2gufgfjls8ekk": 121.55test and 0.6666666test1
-	// So, we should be fetching 156test (34.2 + 121.55 = 155.75 rounded to 156) and 1test1 (0.6333333 + 0.04999995 =  0.68333325 rounded to 1)
+	// (for contract "1") "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt": 30.8test and 0.0666666test1
+	// (for contract "2" and "3") "archway1j08452mqwadp8xu25kn9rleyl2gufgfjls8ekk": 109.65test and 0.6666666test1
+	// So, we should be fetching 141test (30.8 + 109.65 = 140.45 rounded to 141) and 1test1 (0.6333333 + 0.04999995 =  0.68333325 rounded to 1)
 	// from the fee collector
-	// Since, left over threshold is hard coded to 1, we should be transferring 34test to "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt" and
-	// 121test to "archway1j08452mqwadp8xu25kn9rleyl2gufgfjls8ekk" and left over rewards should be 0.2test,0.0666666test1 and 0.55test and 0.68333325test1
+	// Since, left over threshold is hard coded to 1, we should be transferring 30test to "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt" and
+	// 109test to "archway1j08452mqwadp8xu25kn9rleyl2gufgfjls8ekk" and left over rewards should be 0.8test,0.0666666test1 and 0.65test and 0.68333325test1
 	// respectively.
 
 	// Let's check reward keeper call logs first
@@ -222,31 +237,31 @@ func TestBlockTracking(t *testing.T) {
 		Method:       "SendCoinsFromModuleToModule",
 		senderModule: authTypes.FeeCollectorName,
 		recipientModule: gstTypes.ContractRewardCollector,
-		amt: sdk.NewCoins(sdk.NewCoin("test", sdk.NewInt(156)), sdk.NewCoin("test1", sdk.NewInt(1))),
+		amt: sdk.NewCoins(sdk.NewCoin("test", sdk.NewInt(141)), sdk.NewCoin("test1", sdk.NewInt(1))),
 	}, testRewardKeeper.Logs[0])
 	require.Equal(t, &RewardTransferKeeperCallLogs{
 		Method: "SendCoinsFromModuleToAccount",
 		senderModule: gstTypes.ContractRewardCollector,
 		recipientAddr: "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt",
-		amt: sdk.NewCoins(sdk.NewCoin("test", sdk.NewInt(34))),
+		amt: sdk.NewCoins(sdk.NewCoin("test", sdk.NewInt(30))),
 	}, testRewardKeeper.Logs[1])
 	require.Equal(t, &RewardTransferKeeperCallLogs{
 		Method: "SendCoinsFromModuleToAccount",
 		senderModule: gstTypes.ContractRewardCollector,
 		recipientAddr: "archway1j08452mqwadp8xu25kn9rleyl2gufgfjls8ekk",
-		amt: sdk.NewCoins(sdk.NewCoin("test", sdk.NewInt(121))),
+		amt: sdk.NewCoins(sdk.NewCoin("test", sdk.NewInt(109))),
 	}, testRewardKeeper.Logs[2])
 
 	// Let's check left-over balances
 	leftOverEntry, err := keeper.GetLeftOverRewardEntry(ctx, "archway16w95tw2ueqdy0nvknkjv07zc287earxhwlykpt")
 	require.NoError(t, err, "We should be able to get left over entry")
 	require.Equal(t, 2, len(leftOverEntry.ContractRewards))
-	require.Equal(t, sdk.NewDecCoinFromDec("test", sdk.MustNewDecFromStr("0.2")), *leftOverEntry.ContractRewards[0])
+	require.Equal(t, sdk.NewDecCoinFromDec("test", sdk.MustNewDecFromStr("0.8")), *leftOverEntry.ContractRewards[0])
 	require.Equal(t, sdk.NewDecCoinFromDec("test1", sdk.MustNewDecFromStr("0.066666666666666667")), *leftOverEntry.ContractRewards[1])
 
 	leftOverEntry, err = keeper.GetLeftOverRewardEntry(ctx, "archway1j08452mqwadp8xu25kn9rleyl2gufgfjls8ekk")
 	require.NoError(t, err, "We should be able to get left over entry")
 	require.Equal(t, 2, len(leftOverEntry.ContractRewards))
-	require.Equal(t, sdk.NewDecCoinFromDec("test", sdk.MustNewDecFromStr("0.55")), *leftOverEntry.ContractRewards[0])
+	require.Equal(t, sdk.NewDecCoinFromDec("test", sdk.MustNewDecFromStr("0.65")), *leftOverEntry.ContractRewards[0])
 	require.Equal(t, sdk.NewDecCoinFromDec("test1", sdk.MustNewDecFromStr("0.683333333333333333")), *leftOverEntry.ContractRewards[1])
 }
