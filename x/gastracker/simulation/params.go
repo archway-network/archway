68a80f48c3293b35d65312ecc776a28cf6d58321
Frojdi Dymylja <33157909+fdymylja@users.noreply.github.com>
2022-07-12 20:18:28 +0200

    change: simplify parameters usage (#181)
    * change: simplify parameters usage
    
    * fix: ineffectual marshalling

--- x/gastracker/simulation/params.go
+++ x/gastracker/simulation/params.go
@@ -19,21 +19,6 @@ func ParamChanges(r *rand.Rand, cdc codec.Codec) []simtypes.ParamChange {
 				return fmt.Sprintf(`"%v"`, params.ContractPremiumSwitch)
 			},
 		),
-		simulation.NewSimParamChange(gastracker.ModuleName, string(gastracker.ParamsKeyGasRebateToUserSwitch),
-			func(r *rand.Rand) string {
-				return fmt.Sprintf(`"%v"`, params.GasRebateSwitch)
-			},
-		),
-		simulation.NewSimParamChange(gastracker.ModuleName, string(gastracker.ParamsKeyDappInflationRewards),
-			func(r *rand.Rand) string {
-				return fmt.Sprintf(`"%v"`, params.GasDappInflationRewardsSwitch)
-			},
-		),
-		simulation.NewSimParamChange(gastracker.ModuleName, string(gastracker.ParamsKeyGasRebateSwitch),
-			func(r *rand.Rand) string {
-				return fmt.Sprintf(`"%v"`, params.GasRebateSwitch)
-			},
-		),
 		simulation.NewSimParamChange(gastracker.ModuleName, string(gastracker.ParamsKeyGasTrackingSwitch),
 			func(r *rand.Rand) string {
 				return fmt.Sprintf(`"%v"`, params.GasTrackingSwitch)
@@ -44,10 +29,8 @@ func ParamChanges(r *rand.Rand, cdc codec.Codec) []simtypes.ParamChange {
 
 func RandomParams(r *rand.Rand) gastracker.Params {
 	return gastracker.Params{
-		GasTrackingSwitch:             simtypes.RandIntBetween(r, 1, 50)%2 == 0,
-		GasDappInflationRewardsSwitch: simtypes.RandIntBetween(r, 3, 52)%2 == 0,
-		GasRebateSwitch:               simtypes.RandIntBetween(r, 5, 54)%2 == 0,
-		GasRebateToUserSwitch:         simtypes.RandIntBetween(r, 7, 56)%2 == 0,
-		ContractPremiumSwitch:         simtypes.RandIntBetween(r, 9, 58)%2 == 0,
+		GasTrackingSwitch:     simtypes.RandIntBetween(r, 1, 50)%2 == 0,
+		GasRebateToUserSwitch: simtypes.RandIntBetween(r, 7, 56)%2 == 0,
+		ContractPremiumSwitch: simtypes.RandIntBetween(r, 9, 58)%2 == 0,
 	}
 }
