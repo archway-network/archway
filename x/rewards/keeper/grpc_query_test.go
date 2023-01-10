360d3f85dd978924121e4e860a83b7fb8584bde1
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-01-10 13:35:11 +0000

    adding `TestGRPC_ContractMetadata`

--- x/rewards/keeper/grpc_query_test.go
+++ x/rewards/keeper/grpc_query_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	e2eTesting "github.com/archway-network/archway/e2e/testing"
+	"github.com/archway-network/archway/pkg/testutils"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -32,5 +34,47 @@ func (s *KeeperTestSuite) TestGRPC_Params() {
 		s.Require().Equal(params.InflationRewardsRatio, res.Params.InflationRewardsRatio)
 		s.Require().Equal(params.TxFeeRebateRatio, res.Params.TxFeeRebateRatio)
 		s.Require().Equal(params.MaxWithdrawRecords, res.Params.MaxWithdrawRecords)
+	})
+}
+
+func (s *KeeperTestSuite) TestGRPC_ContractMetadata() {
+	ctx, k := s.chain.GetContext(), s.chain.GetApp().RewardsKeeper
+	querySrvr := keeper.NewQueryServer(k)
+	contractViewer := testutils.NewMockContractViewer()
+	k.SetContractInfoViewer(contractViewer)
+	contractAddr := e2eTesting.GenContractAddresses(2)
+	contractAdminAcc := s.chain.GetAccount(0)
+	contractViewer.AddContractAdmin(contractAddr[0].String(), contractAdminAcc.Address.String())
+	contractMeta := rewardsTypes.ContractMetadata{
+		ContractAddress: contractAddr[0].String(),
+		OwnerAddress:    contractAdminAcc.Address.String(),
+	}
+	err := k.SetContractMetadata(ctx, contractAdminAcc.Address, contractAddr[0], contractMeta)
+	s.Require().NoError(err)
+
+	s.Run("err: empty request", func() {
+		_, err := querySrvr.ContractMetadata(sdk.WrapSDKContext(ctx), nil)
+		s.Require().Error(err)
+		s.Require().Equal(status.Error(codes.InvalidArgument, "empty request"), err)
+	})
+
+	s.Run("err: invalid contract address", func() {
+		_, err := querySrvr.ContractMetadata(sdk.WrapSDKContext(ctx), &rewardsTypes.QueryContractMetadataRequest{ContractAddress: "👻"})
+		s.Require().Error(err)
+		s.Require().Equal(status.Error(codes.InvalidArgument, "invalid contract address: decoding bech32 failed: invalid bech32 string length 4"), err)
+	})
+
+	s.Run("err: contract metadata not found", func() {
+		_, err := querySrvr.ContractMetadata(sdk.WrapSDKContext(ctx), &rewardsTypes.QueryContractMetadataRequest{ContractAddress: contractAddr[1].String()})
+		s.Require().Error(err)
+		s.Require().Equal(status.Errorf(codes.NotFound, "metadata for the contract: not found"), err)
+	})
+
+	s.Run("ok: gets contract metadata", func() {
+		res, err := querySrvr.ContractMetadata(sdk.WrapSDKContext(ctx), &rewardsTypes.QueryContractMetadataRequest{ContractAddress: contractAddr[0].String()})
+		s.Require().NoError(err)
+		s.Require().Equal(contractMeta.ContractAddress, res.Metadata.ContractAddress)
+		s.Require().Equal(contractMeta.RewardsAddress, res.Metadata.RewardsAddress)
+		s.Require().Equal(contractMeta.OwnerAddress, res.Metadata.OwnerAddress)
 	})
 }
