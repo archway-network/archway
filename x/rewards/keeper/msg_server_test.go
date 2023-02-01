8ef1efe8c53545a21947bb057c3b1f47fe40b71b
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-02-01 15:37:15 +0000

    adding tests

--- x/rewards/keeper/msg_server_test.go
+++ x/rewards/keeper/msg_server_test.go
@@ -227,3 +227,117 @@ func (s *KeeperTestSuite) TestMsgServer_WithdrawRewards() {
 		})
 	}
 }
+
+func (s *KeeperTestSuite) TestMsgServer_SetFlatFee() {
+	ctx, k := s.chain.GetContext(), s.chain.GetApp().RewardsKeeper
+	contractAdminAcc, otherAcc := s.chain.GetAccount(0), s.chain.GetAccount(1)
+	contractViewer := testutils.NewMockContractViewer()
+	k.SetContractInfoViewer(contractViewer)
+	contractAddr := e2eTesting.GenContractAddresses(1)[0]
+
+	server := keeper.NewMsgServer(k)
+
+	testCases := []struct {
+		testCase    string
+		prepare     func() *rewardstypes.MsgSetFlatFee
+		expectError bool
+		errorType   error
+	}{
+		{
+			testCase: "err: empty request",
+			prepare: func() *rewardstypes.MsgSetFlatFee {
+				return nil
+			},
+			expectError: true,
+			errorType:   status.Error(codes.InvalidArgument, "empty request"),
+		},
+		{
+			testCase: "err: invalid sender address",
+			prepare: func() *rewardstypes.MsgSetFlatFee {
+				return &rewardstypes.MsgSetFlatFee{
+					SenderAddress: "👻",
+				}
+			},
+			expectError: true,
+			errorType:   fmt.Errorf("decoding bech32 failed: invalid bech32 string length 4"),
+		},
+		{
+			testCase: "err: invalid contract address",
+			prepare: func() *rewardstypes.MsgSetFlatFee {
+				return &rewardstypes.MsgSetFlatFee{
+					SenderAddress:   contractAdminAcc.Address.String(),
+					ContractAddress: "👻",
+				}
+			},
+			expectError: true,
+			errorType:   fmt.Errorf("decoding bech32 failed: invalid bech32 string length 4"),
+		},
+		{
+			testCase: "err: contract metadata not exist",
+			prepare: func() *rewardstypes.MsgSetFlatFee {
+				return &rewardstypes.MsgSetFlatFee{
+					SenderAddress:   contractAdminAcc.Address.String(),
+					ContractAddress: contractAddr.String(),
+				}
+			},
+			expectError: true,
+			errorType:   rewardstypes.ErrMetadataNotFound,
+		},
+		{
+			testCase: "err: the message sender is not the contract owner",
+			prepare: func() *rewardstypes.MsgSetFlatFee {
+				contractViewer.AddContractAdmin(contractAddr.String(), contractAdminAcc.Address.String())
+				contractMetadata := rewardstypes.ContractMetadata{
+					ContractAddress: contractAddr.String(),
+					OwnerAddress:    contractAdminAcc.Address.String(),
+					RewardsAddress:  otherAcc.Address.String(),
+				}
+				err := k.SetContractMetadata(ctx, contractAdminAcc.Address, contractAddr, contractMetadata)
+				s.Require().NoError(err)
+
+				return &rewardstypes.MsgSetFlatFee{
+					SenderAddress:   otherAcc.Address.String(),
+					ContractAddress: contractAddr.String(),
+					FlatFeeAmount:   sdk.NewInt64Coin("token", 10),
+				}
+			},
+			expectError: true,
+			errorType:   sdkErrors.Wrap(rewardstypes.ErrUnauthorized, "flat_fee can only be set or changed by the contract owner"),
+		},
+		{
+			testCase: "ok: all good'",
+			prepare: func() *rewardstypes.MsgSetFlatFee {
+				contractViewer.AddContractAdmin(contractAddr.String(), contractAdminAcc.Address.String())
+				contractMetadata := rewardstypes.ContractMetadata{
+					ContractAddress: contractAddr.String(),
+					OwnerAddress:    contractAdminAcc.Address.String(),
+					RewardsAddress:  otherAcc.Address.String(),
+				}
+				err := k.SetContractMetadata(ctx, contractAdminAcc.Address, contractAddr, contractMetadata)
+				s.Require().NoError(err)
+
+				return &rewardstypes.MsgSetFlatFee{
+					SenderAddress:   contractAdminAcc.Address.String(),
+					ContractAddress: contractAddr.String(),
+					FlatFeeAmount:   sdk.NewInt64Coin("token", 10),
+				}
+			},
+			expectError: false,
+			errorType:   nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(fmt.Sprintf("Case: %s", tc.testCase), func() {
+			req := tc.prepare()
+			res, err := server.SetFlatFee(sdk.WrapSDKContext(ctx), req)
+			if tc.expectError {
+				s.Require().Error(err)
+				s.Require().Equal(tc.errorType.Error(), err.Error())
+			} else {
+				s.Require().NoError(err)
+				s.Require().Equal(&rewardstypes.MsgSetFlatFeeResponse{}, res)
+			}
+		})
+	}
+}
