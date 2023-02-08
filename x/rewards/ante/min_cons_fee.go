fadbef63f5c2bbb41f5a2ba6e9b022f7bf0ea79f
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-02-08 11:10:23 +0000

    ante check if the msg is a cw exectue tx and check if contract has flat fee

--- x/rewards/ante/min_cons_fee.go
+++ x/rewards/ante/min_cons_fee.go
@@ -1,15 +1,18 @@
 package ante
 
 import (
+	wasmTypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/authz"
 
 	"github.com/archway-network/archway/pkg"
 )
 
-// MinConsensusFeeReaderExpected defines the expected interface for the x/rewards keeper.
-type MinConsensusFeeReaderExpected interface {
+// RewardsFeeReaderExpected defines the expected interface for the x/rewards keeper.
+type RewardsFeeReaderExpected interface {
 	GetMinConsensusFee(ctx sdk.Context) (sdk.DecCoin, bool)
+	GetFlatFee(ctx sdk.Context, contractAddr sdk.AccAddress) (sdk.Coin, bool)
 }
 
 // MinFeeDecorator rejects transaction if its fees are less than minimum fees defined by the x/rewards module.
@@ -17,11 +20,11 @@ type MinConsensusFeeReaderExpected interface {
 // The minimum consensus fee value is defined by block dApp rewards and rewards distribution parameters.
 // CONTRACT: Tx must implement FeeTx interface to use MinFeeDecorator.
 type MinFeeDecorator struct {
-	rewardsKeeper MinConsensusFeeReaderExpected
+	rewardsKeeper RewardsFeeReaderExpected
 }
 
 // NewMinFeeDecorator returns a new MinFeeDecorator instance.
-func NewMinFeeDecorator(rk MinConsensusFeeReaderExpected) MinFeeDecorator {
+func NewMinFeeDecorator(rk RewardsFeeReaderExpected) MinFeeDecorator {
 	return MinFeeDecorator{
 		rewardsKeeper: rk,
 	}
@@ -59,10 +62,42 @@ func (mfd MinFeeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool,
 		Amount: gasUnitPrice.Amount.Mul(txGasLimit).RoundInt(),
 	}
 
+	for _, m := range tx.GetMsgs() {
+		contractAddr, cwExecMsgFound := getMsgContractAddress(m)
+		if cwExecMsgFound {
+			ca := sdk.MustAccAddressFromBech32(contractAddr)
+			flatfee, found := mfd.rewardsKeeper.GetFlatFee(ctx, ca)
+			if found {
+				panic(flatfee)
+			}
+		}
+	}
+
 	// Check (skip if the expected amount is zero)
 	if minFeeExpected.Amount.IsZero() || txFees.IsAnyGTE(sdk.Coins{minFeeExpected}) {
 		return next(ctx, tx, simulate)
 	}
 
 	return ctx, sdkErrors.Wrapf(sdkErrors.ErrInsufficientFee, "tx fee %s is less than min fee: %s", txFees, minFeeExpected)
+}
+
+func getMsgContractAddress(m sdk.Msg) (contractAddress string, isContractExecute bool) {
+	switch msg := m.(type) {
+	case *wasmTypes.MsgExecuteContract:
+		{
+			return msg.Contract, true
+		}
+	case *authz.MsgExec:
+		{
+			// todo: decode authz msgs and check if they are execute contract
+			// var wrappedMsg sdk.Msg
+			// err := dec.codec.UnpackAny(msg, &wrappedMsg)
+			// if err != nil {
+			// 	return sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "error decoding authz messages")
+			// }
+			return "", false
+		}
+	default:
+		return "", false
+	}
 }
