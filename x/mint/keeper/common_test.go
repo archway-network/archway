607d331b47fb71b0f6abdaa554537d2f22faf2a9
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-02-23 17:49:17 +0000

    cleanup

--- x/mint/keeper/common_test.go
+++ x/mint/keeper/common_test.go
@@ -3,10 +3,6 @@ package keeper_test
 import (
 	"testing"
 
-	"github.com/archway-network/archway/app"
-	e2eTesting "github.com/archway-network/archway/e2e/testing"
-	"github.com/archway-network/archway/x/mint/keeper"
-	"github.com/archway-network/archway/x/mint/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
@@ -14,25 +10,15 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
 	"github.com/stretchr/testify/require"
-	"github.com/stretchr/testify/suite"
 	"github.com/tendermint/spm/cosmoscmd"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
+
+	"github.com/archway-network/archway/app"
+	"github.com/archway-network/archway/x/mint/keeper"
+	"github.com/archway-network/archway/x/mint/types"
 )
-
-type KeeperTestSuite struct {
-	suite.Suite
-	chain *e2eTesting.TestChain
-}
-
-func (s *KeeperTestSuite) SetupTest() {
-	s.chain = e2eTesting.NewTestChain(s.T(), 1)
-}
-
-func TestMintKeeper(t *testing.T) {
-	suite.Run(t, new(KeeperTestSuite))
-}
 
 func SetupTestMintKeeper(t testing.TB) (keeper.Keeper, sdk.Context) {
 	encoding := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
