a7b8ffae814a1ceb1456643a766b0fb2d808c612
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-02-24 14:11:30 +0000

    implementing `Params.Validate()`

--- x/mint/types/errors.go
+++ x/mint/types/errors.go
@@ -3,8 +3,12 @@ package types
 import sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 var (
-	DefaultCodespace    = ModuleName
-	ErrInternal         = sdkErrors.Register(DefaultCodespace, 0, "internal error") // smth went wrong
-	ErrInvalidInflation = sdkErrors.Register(DefaultCodespace, 1, "invalid inflation percentage")
-	ErrInvalidTimestamp = sdkErrors.Register(DefaultCodespace, 2, "invalid last block info timestamp")
+	DefaultCodespace                = ModuleName
+	ErrInternal                     = sdkErrors.Register(DefaultCodespace, 0, "internal error") // smth went wrong
+	ErrInvalidInflation             = sdkErrors.Register(DefaultCodespace, 1, "invalid inflation percentage")
+	ErrInvalidTimestamp             = sdkErrors.Register(DefaultCodespace, 2, "invalid last block info timestamp")
+	ErrInvalidBondedRatio           = sdkErrors.Register(DefaultCodespace, 3, "invalid bonded ratio")
+	ErrInvalidInflationChange       = sdkErrors.Register(DefaultCodespace, 4, "invalid inflation change ratio")
+	ErrInvalidInflationRecipient    = sdkErrors.Register(DefaultCodespace, 5, "invalid inflation recipient")
+	ErrInvalidInflationDistribution = sdkErrors.Register(DefaultCodespace, 6, "invalid inflation distribution")
 )
--- x/mint/types/params.go
+++ x/mint/types/params.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // NewParams creates a new Params instance.
@@ -16,5 +17,66 @@ func DefaultParams() Params {
 
 // Validate perform object fields validation.
 func (m Params) Validate() error {
-	panic("unimplemented 👻")
+	if err := validateInflation(m.MinInflation); err != nil {
+		return sdkErrors.Wrap(err, "min_inflation param has invalid value, should be between 0 and 1")
+	}
+	if err := validateInflation(m.MaxInflation); err != nil {
+		return sdkErrors.Wrap(err, "max_inflation param has invalid value, should be between 0 and 1")
+	}
+	if err := validateBondedRatio(m.MinBonded); err != nil {
+		return sdkErrors.Wrap(err, "min_bonded param has invalid value, should be between 0 and 1")
+	}
+	if err := validateBondedRatio(m.MaxBonded); err != nil {
+		return sdkErrors.Wrap(err, "max_bonded param has invalid value, should be between 0 and 1")
+	}
+	if err := validateInflationChange(m.InflationChange); err != nil {
+		return sdkErrors.Wrap(err, "inflation_change param has invalid value, should be between 0 and 1")
+	}
+	if err := validateInflationRecipients(m.InflationRecipients); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateInflation(inflation sdk.Dec) error {
+	if !isDecValidPercentage(inflation) {
+		return ErrInvalidInflation
+	}
+	return nil
+}
+
+func validateBondedRatio(bondedRatio sdk.Dec) error {
+	if !isDecValidPercentage(bondedRatio) {
+		return ErrInvalidBondedRatio
+	}
+	return nil
+}
+
+func validateInflationChange(inflationChange sdk.Dec) error {
+	if !isDecValidPercentage(inflationChange) {
+		return ErrInvalidInflationChange
+	}
+	return nil
+}
+
+func validateInflationRecipients(inflationRecipients []*Params_InflationRecipient) error {
+	if len(inflationRecipients) < 1 {
+		return sdkErrors.Wrap(ErrInvalidInflationRecipient, "inflation recipients not found")
+	}
+	inflationDistribution := sdk.ZeroDec()
+	for _, recipient := range inflationRecipients {
+		inflationDistribution = inflationDistribution.Add(recipient.Ratio)
+	}
+	if !inflationDistribution.Equal(sdk.OneDec()) {
+		return sdkErrors.Wrap(ErrInvalidInflationDistribution, "inflation distribution sum is not equal to 1")
+	}
+	return nil
+}
+
+// isDecValidPercentage returns true if the given sdk.Dec value is between 0 and 1
+func isDecValidPercentage(dec sdk.Dec) bool {
+	if dec.LT(sdk.ZeroDec()) || dec.GT(sdk.OneDec()) {
+		return false
+	}
+	return true
 }
