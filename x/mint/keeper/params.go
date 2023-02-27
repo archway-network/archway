020eb8ed3acdd2992b1e331d60c16b021c6520ce
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-02-27 15:35:21 +0000

    adding param keepers

--- x/mint/keeper/params.go
+++ x/mint/keeper/params.go
@@ -1,1 +1,19 @@
 package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/archway-network/archway/x/mint/types"
+)
+
+// SetParams sets the total set of minting parameters.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramStore.SetParamSet(ctx, &params)
+}
+
+// GetParams returns the total set of minting parameters.
+func (k Keeper) GetParams(ctx sdk.Context) types.Params {
+	var params types.Params
+	k.paramStore.GetParamSet(ctx, &params)
+	return params
+}
