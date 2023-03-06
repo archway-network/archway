d8fefc76ad588eb2e35a8f1f2391be33507847a2
Vidhan <vidhanarya50@gmail.com>
2023-03-06 22:09:30 +0530

    Add Mint Query CLI (#306)

--- x/mint/client/cli/query.go
+++ x/mint/client/cli/query.go
@@ -1,10 +1,75 @@
 package cli
 
 import (
+	"github.com/archway-network/archway/x/mint/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 )
 
 // GetQueryCmd builds query command group for the module.
 func GetQueryCmd() *cobra.Command {
-	panic("unimplemented 👻")
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the mint module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+	cmd.AddCommand(
+		getQueryParamsCmd(),
+		getQueryInflationCmd(),
+	)
+
+	return cmd
+}
+
+func getQueryParamsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query module parameters",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(&res.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+func getQueryInflationCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "inflation",
+		Args:  cobra.NoArgs,
+		Short: "Query inflation for the last block",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Inflation(cmd.Context(), &types.QueryInflationRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
 }
