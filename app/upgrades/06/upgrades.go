908877621720574dfaea24d61ac3850f613b0cc7
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-05-22 14:41:04 +0100

    adding v0.6 upgrade handler

---
+++ app/upgrades/06/upgrades.go
@@ -0,0 +1,25 @@
+package upgrade06
+
+import (
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/archway-network/archway/app/upgrades"
+	ibcfeetypes "github.com/cosmos/ibc-go/v4/modules/apps/29-fee/types"
+)
+
+const Name = "v0.6.0"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName: Name,
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
+		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return mm.RunMigrations(ctx, cfg, fromVM)
+		}
+	},
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added: []string{ibcfeetypes.ModuleName},
+	},
+}
