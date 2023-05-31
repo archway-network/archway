b6de0aa2b2e7111cdda05fed710cf1db61156e9b
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-05-31 10:47:22 +0100

    remove the upgrade handlers

--- app/app_upgrades.go
+++ app/app_upgrades.go
@@ -6,20 +6,11 @@ import (
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/archway-network/archway/app/upgrades"
-	upgrade_0_3 "github.com/archway-network/archway/app/upgrades/03"
-	upgrade_0_4 "github.com/archway-network/archway/app/upgrades/04"
-	upgrade_0_5_2 "github.com/archway-network/archway/app/upgrades/052"
-	upgrade_0_6 "github.com/archway-network/archway/app/upgrades/06"
 )
 
 // UPGRADES
 
-var Upgrades = []upgrades.Upgrade{
-	upgrade_0_3.Upgrade,   // v0.3.0
-	upgrade_0_4.Upgrade,   // v0.4.0
-	upgrade_0_5_2.Upgrade, // v0.5.2
-	upgrade_0_6.Upgrade,   // v0.6.0
-}
+var Upgrades = []upgrades.Upgrade{}
 
 func (app *ArchwayApp) setupUpgrades() {
 	app.setUpgradeHandlers()
