a93f312379a9bb6b0adb8f15dd128bc6bf73d5d2
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-07-06 10:12:09 +0200

    updating upgrade handlers

--- app/upgrades/2_0_0/upgrades.go
+++ app/upgrades/2_0_0/upgrades.go
@@ -7,6 +7,10 @@ import (
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/archway-network/archway/app/upgrades"
+	ica "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts"
+	icacontrollertypes "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts/controller/types"
+	icahosttypes "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts/host/types"
+	icatypes "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts/types"
 )
 
 const Name = "v2.0.0"
@@ -15,8 +19,40 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName: Name,
 	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
 		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+			// Set Initial Consensus Version
+			fromVM[icatypes.ModuleName] = mm.Modules[icatypes.ModuleName].ConsensusVersion()
+			// create ICS27 Controller submodule params
+			controllerParams := icacontrollertypes.Params{}
+			// create ICS27 Host submodule params
+			hostParams := icahosttypes.Params{
+				HostEnabled:   true,
+				AllowMessages: []string{
+					// sdk.MsgTypeURL(&banktypes.MsgSend{}),
+					// sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}),
+					// sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}),
+					// sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}),
+					// sdk.MsgTypeURL(&stakingtypes.MsgCreateValidator{}),
+					// sdk.MsgTypeURL(&stakingtypes.MsgEditValidator{}),
+					// sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}),
+					// sdk.MsgTypeURL(&distrtypes.MsgSetWithdrawAddress{}),
+					// sdk.MsgTypeURL(&distrtypes.MsgWithdrawValidatorCommission{}),
+					// sdk.MsgTypeURL(&distrtypes.MsgFundCommunityPool{}),
+					// sdk.MsgTypeURL(&govtypes.MsgVote{}),
+				},
+			}
+
+			icamodule, ok := mm.Modules[icatypes.ModuleName].(ica.AppModule)
+			if !ok {
+				panic("module is not of type ica.AppModule")
+			}
+			// initialize ICS27 module
+			icamodule.InitModule(ctx, controllerParams, hostParams)
+
 			return mm.RunMigrations(ctx, cfg, fromVM)
 		}
 	},
-	StoreUpgrades: storetypes.StoreUpgrades{},
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added: []string{icahosttypes.StoreKey},
+	},
 }
