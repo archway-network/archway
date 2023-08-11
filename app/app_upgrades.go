ef667b5d003d70845bcc57d7be8ff77ce0da0eb6
Zanicar <zanicar@noctranet.com>
2023-08-11 13:19:59 +0200

    feat: Add version 3 upgrade handler

--- app/app_upgrades.go
+++ app/app_upgrades.go
@@ -9,6 +9,7 @@ import (
 	upgrade_0_6 "github.com/archway-network/archway/app/upgrades/06"
 	upgrade1_0_0_rc_4 "github.com/archway-network/archway/app/upgrades/1_0_0_rc_4"
 	upgrade2_0_0 "github.com/archway-network/archway/app/upgrades/2_0_0"
+	upgrade3_0_0 "github.com/archway-network/archway/app/upgrades/3_0_0"
 )
 
 // UPGRADES
@@ -17,6 +18,7 @@ var Upgrades = []upgrades.Upgrade{
 	upgrade_0_6.Upgrade,       // v0.6.0
 	upgrade1_0_0_rc_4.Upgrade, // v1.0.0-rc.4
 	upgrade2_0_0.Upgrade,      // v2.0.0
+	upgrade3_0_0.Upgrade,      // v3.0.0
 }
 
 func (app *ArchwayApp) setupUpgrades() {
