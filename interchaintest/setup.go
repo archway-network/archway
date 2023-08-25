f4f66402f66c42b57e4e59e354c413530dff13fa
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-08-25 09:49:24 +0530

    updating chain upgrade test for v4 release

--- interchaintest/setup.go
+++ interchaintest/setup.go
@@ -8,8 +8,8 @@ import (
 )
 
 const (
-	initialVersion = "v3.0.0" // The last release of the chain. The one the mainnet is running on
-	upgradeName    = "v4.0.0" // The next upgrade name. Should match the upgrade handler.
+	initialVersion = "v4.0.0" // The last release of the chain. The one the mainnet is running on
+	upgradeName    = "latest" // The next upgrade name. Should match the upgrade handler.
 	chainName      = "archway"
 )
 
