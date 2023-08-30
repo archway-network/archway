91ca9dcd1a316ee5c1b3af6db8527f33e3ddf2e5
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-08-30 20:49:25 +0530

    adding the upgrade handler to provide burn permission to feecollector module account

--- app/upgrades/latest/upgrades.go
+++ app/upgrades/latest/upgrades.go
@@ -1,10 +1,13 @@
 package upgradelatest
 
 import (
+	"fmt"
+
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/archway-network/archway/app/upgrades"
@@ -16,8 +19,16 @@ const Name = "latest"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName: Name,
-	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator, appKeepers keeper.AccountKeeper) upgradetypes.UpgradeHandler {
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator, accountKeeper keeper.AccountKeeper) upgradetypes.UpgradeHandler {
 		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			fcAccount := accountKeeper.GetModuleAccount(ctx, authtypes.FeeCollectorName)
+			account, ok := fcAccount.(*authtypes.ModuleAccount)
+			if !ok {
+				return nil, fmt.Errorf("feeCollector account is not *authtypes.ModuleAccount")
+			}
+			account.Permissions = append(account.Permissions, authtypes.Burner)
+			accountKeeper.SetModuleAccount(ctx, account)
+
 			return mm.RunMigrations(ctx, cfg, fromVM)
 		}
 	},
