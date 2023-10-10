73c850e0864ffaabbadaa020086bee60c2893910
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-10-10 11:10:02 +0530

    adding module migrations forx/rewards

---
+++ x/rewards/migrations/v2/migrate.go
@@ -0,0 +1,33 @@
+package v2
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+
+	"github.com/archway-network/archway/x/rewards/types"
+)
+
+// MigrateStore migrates the x/rewards module state from the consensus version 1 to
+// version 2. Specifically, it takes the parameters that are currently stored
+// and managed by the x/params module and stores them directly into the x/distribution
+// module state.
+func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, legacySubspace paramtypes.Subspace, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+	var currParams types.Params
+	legacySubspace.GetParamSet(ctx, &currParams)
+
+	if err := currParams.Validate(); err != nil {
+		return err
+	}
+
+	bz, err := cdc.Marshal(&currParams)
+	if err != nil {
+		return err
+	}
+
+	store.Set(types.ParamsKey, bz)
+
+	return nil
+}
