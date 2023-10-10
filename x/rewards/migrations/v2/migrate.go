17b8a3ac20153825a3d04dfc88ec24116e4a8b85
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-10-10 11:26:05 +0530

    adding migratestore test

--- x/rewards/migrations/v2/migrate.go
+++ x/rewards/migrations/v2/migrate.go
@@ -4,16 +4,16 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
+	"github.com/archway-network/archway/x/rewards/exported"
 	"github.com/archway-network/archway/x/rewards/types"
 )
 
 // MigrateStore migrates the x/rewards module state from the consensus version 1 to
 // version 2. Specifically, it takes the parameters that are currently stored
-// and managed by the x/params module and stores them directly into the x/distribution
+// and managed by the x/params module and stores them directly into the x/rewards
 // module state.
-func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, legacySubspace paramtypes.Subspace, cdc codec.BinaryCodec) error {
+func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, legacySubspace exported.Subspace, cdc codec.BinaryCodec) error {
 	store := ctx.KVStore(storeKey)
 	var currParams types.Params
 	legacySubspace.GetParamSet(ctx, &currParams)
