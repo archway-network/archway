c9bb51c6605d317ee70f2c457261bc5a3ea693e0
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-11-03 10:23:52 +0000

    adding params types

--- x/callback/types/params.go
+++ x/callback/types/params.go
@@ -1,20 +1,57 @@
 package types
 
 import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	fmt "fmt"
+
+	"cosmossdk.io/math"
+)
+
+var (
+	DefaultCallbackGasLimit               = uint64(1000000)
+	DefaultMaxBlockReservationLimit       = uint64(3)
+	DefaultMaxFutureReservationLimit      = uint64(10000)
+	DefaultBlockReservationFeeMultiplier  = math.LegacyMustNewDecFromStr("1.0")
+	DefaultFutureReservationFeeMultiplier = math.LegacyMustNewDecFromStr("1.0")
 )
 
 // NewParams creates a new Params instance.
-func NewParams(inflationRewardsRatio, txFeeRebateRatio sdk.Dec, maxwithdrawRecords uint64) Params {
-	panic("unimplemented 👻")
+func NewParams(
+	callbackGasLimit uint64,
+	maxBlockReservationLimit uint64,
+	maxFutureReservationLimit uint64,
+	blockReservationFeeMultiplier math.LegacyDec,
+	futureReservationFeeMultiplier math.LegacyDec,
+) Params {
+	return Params{
+		CallbackGasLimit:               callbackGasLimit,
+		MaxBlockReservationLimit:       maxBlockReservationLimit,
+		MaxFutureReservationLimit:      maxFutureReservationLimit,
+		BlockReservationFeeMultiplier:  &blockReservationFeeMultiplier,
+		FutureReservationFeeMultiplier: &futureReservationFeeMultiplier,
+	}
 }
 
 // DefaultParams returns a default set of parameters.
 func DefaultParams() Params {
-	panic("unimplemented 👻")
+	return NewParams(
+		DefaultCallbackGasLimit,
+		DefaultMaxBlockReservationLimit,
+		DefaultMaxFutureReservationLimit,
+		DefaultBlockReservationFeeMultiplier,
+		DefaultFutureReservationFeeMultiplier,
+	)
 }
 
 // Validate perform object fields validation.
-func (m Params) Validate() error {
-	panic("unimplemented 👻")
+func (p Params) Validate() error {
+	if p.CallbackGasLimit == 0 {
+		return fmt.Errorf("CallbackGasLimit must be greater than 0")
+	}
+	if p.BlockReservationFeeMultiplier.IsNegative() {
+		return fmt.Errorf("BlockReservationFeeMultiplier must be greater than 0")
+	}
+	if p.FutureReservationFeeMultiplier.IsNegative() {
+		return fmt.Errorf("FutureReservationFeeMultiplier must be greater than 0")
+	}
+	return nil
 }
