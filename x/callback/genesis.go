4b1ebf78327c2c8b536568672e59e0589932c7ea
Spoorthi <9302666+spoo-bar@users.noreply.github.com>
2023-12-04 10:24:36 +0000

    feat: Implement genesis methods (#515)
    * removing callbacks from genesis state
    
    * implementing base genesis
    
    * implement init and export genesis
    
    * exporting callbacks in genesis
    
    * exporting callbacks in genesis
    
    * linting

--- x/callback/genesis.go
+++ x/callback/genesis.go
@@ -9,10 +9,22 @@ import (
 
 // InitGenesis initializes the module genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-	panic("unimplemented 👻")
+	params := genState.Params
+	err := k.Params.Set(ctx, params)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // ExportGenesis exports the module genesis for the current block.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	panic("unimplemented 👻")
+	params, err := k.Params.Get(ctx)
+	if err != nil {
+		panic(err)
+	}
+	callbacks, err := k.GetAllCallbacks(ctx)
+	if err != nil {
+		panic(err)
+	}
+	return types.NewGenesisState(params, callbacks)
 }
--- x/callback/types/genesis.go
+++ x/callback/types/genesis.go
@@ -3,16 +3,33 @@ package types
 // NewGenesisState creates a new GenesisState object.
 func NewGenesisState(
 	params Params,
+	callbacks []Callback,
 ) *GenesisState {
-	panic("unimplemented 👻")
+	var callbacksCopy []*Callback
+	for _, c := range callbacks {
+		callbacksCopy = append(callbacksCopy, &c)
+	}
+	return &GenesisState{
+		Params:    params,
+		Callbacks: callbacksCopy,
+	}
 }
 
 // DefaultGenesisState returns a default genesis state.
 func DefaultGenesis() *GenesisState {
-	panic("unimplemented 👻")
+	defaultParams := DefaultParams()
+	return NewGenesisState(defaultParams, nil)
 }
 
 // Validate perform object fields validation.
-func (m GenesisState) Validate() error {
-	panic("unimplemented 👻")
+func (g GenesisState) Validate() error {
+	if err := g.Params.Validate(); err != nil {
+		return err
+	}
+	for _, callback := range g.GetCallbacks() {
+		if err := callback.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
