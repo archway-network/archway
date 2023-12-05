00ecc91f7d97c6ebdf5dd8cfc02b911a85b2f7cb
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-12-05 15:35:24 +0000

    adding event emit helpers

---
+++ x/callback/types/events.go
@@ -0,0 +1,98 @@
+package types
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func EmitCallbackRegisteredEvent(
+	ctx sdk.Context,
+	contractAddress string,
+	jobId uint64,
+	callbackHeight int64,
+	feeSplit *CallbackFeesFeeSplit,
+	reservedBy string,
+) {
+	err := ctx.EventManager().EmitTypedEvent(&CallbackRegisteredEvent{
+		ContractAddress: contractAddress,
+		JobId:           jobId,
+		CallbackHeight:  callbackHeight,
+		FeeSplit:        feeSplit,
+		ReservedBy:      reservedBy,
+	})
+	if err != nil {
+		panic(fmt.Errorf("sending CallbackRegisteredEvent event: %w", err))
+	}
+}
+
+func EmitCallbackCancelledEvent(
+	ctx sdk.Context,
+	contractAddress string,
+	jobId uint64,
+	callbackHeight int64,
+	cancelledBy string,
+	refundAmount sdk.Coin,
+) {
+	err := ctx.EventManager().EmitTypedEvent(&CallbackCancelledEvent{
+		ContractAddress: contractAddress,
+		JobId:           jobId,
+		CallbackHeight:  callbackHeight,
+		CancelledBy:     cancelledBy,
+		RefundAmount:    refundAmount,
+	})
+	if err != nil {
+		panic(fmt.Errorf("sending CallbackCancelledEvent event: %w", err))
+	}
+}
+
+func EmitCallbackExecutedSuccessEvent(
+	ctx sdk.Context,
+	contractAddress string,
+	jobId uint64,
+	sudoMsg string,
+	gasUsed uint64,
+	response string,
+) {
+	execution := NewCallbackExecuted(contractAddress, jobId, sudoMsg, gasUsed)
+	err := ctx.EventManager().EmitTypedEvent(&CallbackExecutedSuccessEvent{
+		Response:  response,
+		Execution: &execution,
+	})
+	if err != nil {
+		panic(fmt.Errorf("sending CallbackExecutedSuccessEvent event: %w", err))
+	}
+}
+
+func EmitCallbackExecutedFailedEvent(
+	ctx sdk.Context,
+	contractAddress string,
+	jobId uint64,
+	sudoMsg string,
+	gasUsed uint64,
+	errMsg string,
+) {
+	execution := NewCallbackExecuted(contractAddress, jobId, sudoMsg, gasUsed)
+	err := ctx.EventManager().EmitTypedEvent(&CallbackExecutedFailedEvent{
+		Error:     errMsg,
+		Execution: &execution,
+	})
+	if err != nil {
+		panic(fmt.Errorf("sending CallbackExecutedFailedEvent event: %w", err))
+	}
+}
+
+func NewCallbackExecuted(
+	contractAddress string,
+	jobId uint64,
+	sudoMsg string,
+	gasUsed uint64,
+) CallbackExecuted {
+	execution := CallbackExecuted{
+		ContractAddress: contractAddress,
+		JobId:           jobId,
+		SudoMsg:         sudoMsg,
+		GasUsed:         gasUsed,
+	}
+	return execution
+}
