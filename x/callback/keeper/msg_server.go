c9afcce3d6d804017e91e0783948ec89da296298
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-12-07 13:49:32 +0000

    fixing merge

--- x/callback/keeper/msg_server.go
+++ x/callback/keeper/msg_server.go
@@ -66,7 +66,7 @@ func (s MsgServer) CancelCallback(c context.Context, request *types.MsgCancelCal
 		request.GetJobId(),
 		request.GetCallbackHeight(),
 		request.Sender,
-		*txFee,
+		refundFees,
 	)
 
 	return &types.MsgCancelCallbackResponse{
