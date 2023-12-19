bfec41908a36699644287603422bc5ff1f8cd0b8
Spoorthi <spoorthi_s@ymail.com>
2023-12-19 11:52:17 +0000

    adding TestIterateCallbacksByHeight

--- x/callback/keeper/callback_test.go
+++ x/callback/keeper/callback_test.go
@@ -418,3 +418,76 @@ func (s *KeeperTestSuite) TestGetAllCallbacks() {
 		s.Assert().Equal(3, len(callbacks))
 	})
 }
+
+func (s *KeeperTestSuite) TestIterateCallbacksByHeight() {
+	ctx, keeper := s.chain.GetContext().WithBlockHeight(100), s.chain.GetApp().Keepers.CallbackKeeper
+	rewardsKeeper := s.chain.GetApp().Keepers.RewardsKeeper
+	contractViewer := testutils.NewMockContractViewer()
+	keeper.SetWasmKeeper(contractViewer)
+	validCoin := sdk.NewInt64Coin("stake", 10)
+
+	contractAddr := e2eTesting.GenContractAddresses(1)[0]
+	contractAdminAcc := s.chain.GetAccount(0)
+	contractOwnerAcc := s.chain.GetAccount(2)
+
+	contractViewer.AddContractAdmin(
+		contractAddr.String(),
+		contractAdminAcc.Address.String(),
+	)
+
+	var metaCurrent rewardsTypes.ContractMetadata
+	metaCurrent.ContractAddress = contractAddr.String()
+	metaCurrent.OwnerAddress = contractOwnerAcc.Address.String()
+	metaCurrent.RewardsAddress = contractOwnerAcc.Address.String()
+	rewardsKeeper.SetContractInfoViewer(contractViewer)
+	err := rewardsKeeper.SetContractMetadata(ctx, contractAdminAcc.Address, contractAddr, metaCurrent)
+	s.Require().NoError(err)
+
+	callbackHeight := int64(101)
+
+	callback := types.Callback{
+		ContractAddress: contractAddr.String(),
+		JobId:           1,
+		CallbackHeight:  callbackHeight,
+		ReservedBy:      contractAddr.String(),
+		FeeSplit: &types.CallbackFeesFeeSplit{
+			TransactionFees:       &validCoin,
+			BlockReservationFees:  &validCoin,
+			FutureReservationFees: &validCoin,
+			SurplusFees:           &validCoin,
+		},
+	}
+	err = keeper.SaveCallback(ctx, callback)
+	s.Require().NoError(err)
+
+	callback.JobId = 2
+	err = keeper.SaveCallback(ctx, callback)
+	s.Require().NoError(err)
+
+	callback.JobId = 3
+	err = keeper.SaveCallback(ctx, callback)
+	s.Require().NoError(err)
+
+	callback.JobId = 4
+	callback.CallbackHeight = callbackHeight + 1
+	err = keeper.SaveCallback(ctx, callback)
+	s.Require().NoError(err)
+
+	s.Run("OK: Get all three existing callbacks at height 101", func() {
+		count := 0
+		keeper.IterateCallbacksByHeight(ctx, callbackHeight, func(callback types.Callback) bool {
+			count++
+			return false
+		})
+		s.Assert().Equal(3, count)
+	})
+
+	s.Run("OK: Get one existing callbacks at height 102", func() {
+		count := 0
+		keeper.IterateCallbacksByHeight(ctx, callbackHeight+1, func(callback types.Callback) bool {
+			count++
+			return false
+		})
+		s.Assert().Equal(1, count)
+	})
+}
