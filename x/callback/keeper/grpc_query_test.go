380a38a68cc484c5eef57e9e6adf129d727f1821
Spoorthi <spoorthi_s@ymail.com>
2023-12-19 12:47:29 +0000

    implementing TestEstimateCallbackFees

--- x/callback/keeper/grpc_query_test.go
+++ x/callback/keeper/grpc_query_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"fmt"
 
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	e2eTesting "github.com/archway-network/archway/e2e/testing"
@@ -110,6 +111,82 @@ func (s *KeeperTestSuite) TestCallbacks() {
 			} else {
 				s.Require().NoError(err)
 				s.Require().Equal(tc.response, len(res.Callbacks))
+			}
+		})
+	}
+}
+
+func (s *KeeperTestSuite) TestEstimateCallbackFees() {
+	ctx, keeper := s.chain.GetContext().WithBlockHeight(101), s.chain.GetApp().Keepers.CallbackKeeper
+	queryServer := callbackKeeper.NewQueryServer(keeper)
+	zeroCoin := sdk.NewInt64Coin("stake", 0)
+
+	params, err := keeper.GetParams(ctx)
+	s.Require().NoError(err)
+	err = keeper.SetParams(ctx, types.Params{
+		CallbackGasLimit:               0,
+		MaxBlockReservationLimit:       params.MaxBlockReservationLimit,
+		MaxFutureReservationLimit:      params.MaxFutureReservationLimit,
+		FutureReservationFeeMultiplier: math.LegacyMustNewDecFromStr("0"),
+		BlockReservationFeeMultiplier:  math.LegacyMustNewDecFromStr("0"),
+	})
+	s.Require().NoError(err)
+
+	testCases := []struct {
+		testCase    string
+		prepare     func() *types.QueryEstimateCallbackFeesRequest
+		expectError bool
+		response    *types.QueryEstimateCallbackFeesResponse
+	}{
+		{
+			testCase: "FAIL: empty request",
+			prepare: func() *types.QueryEstimateCallbackFeesRequest {
+				return nil
+			},
+			expectError: true,
+			response:    nil,
+		},
+		{
+			testCase: "FAIL: height is in the past",
+			prepare: func() *types.QueryEstimateCallbackFeesRequest {
+				return &types.QueryEstimateCallbackFeesRequest{
+					BlockHeight: 100,
+				}
+			},
+			expectError: true,
+			response:    nil,
+		},
+		{
+			testCase: "OK: fetch fees for next height",
+			prepare: func() *types.QueryEstimateCallbackFeesRequest {
+				return &types.QueryEstimateCallbackFeesRequest{
+					BlockHeight: 101,
+				}
+			},
+			expectError: false,
+			response: &types.QueryEstimateCallbackFeesResponse{
+				FeeSplit: &types.CallbackFeesFeeSplit{
+					TransactionFees:       &zeroCoin,
+					BlockReservationFees:  &zeroCoin,
+					FutureReservationFees: &zeroCoin,
+				},
+				TotalFees: &zeroCoin,
+			},
+		},
+	}
+	for _, tc := range testCases {
+		s.Run(fmt.Sprintf("Case: %s", tc.testCase), func() {
+			req := tc.prepare()
+			res, err := queryServer.EstimateCallbackFees(sdk.WrapSDKContext(ctx), req)
+			if tc.expectError {
+				s.Require().Error(err)
+			} else {
+				s.Require().NoError(err)
+				s.Require().Equal(tc.response.TotalFees, res.TotalFees)
+				s.Require().Equal(tc.response.FeeSplit.BlockReservationFees, res.FeeSplit.BlockReservationFees)
+				s.Require().Equal(tc.response.FeeSplit.FutureReservationFees, res.FeeSplit.FutureReservationFees)
+				s.Require().Equal(tc.response.FeeSplit.TransactionFees, res.FeeSplit.TransactionFees)
+				s.Require().Equal(tc.response.FeeSplit.SurplusFees, res.FeeSplit.SurplusFees)
 			}
 		})
 	}
