11a28736944755ebbd0ff41aff84d3271078401d
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-02-13 16:02:53 +0000

    changing proto path

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -13,15 +13,6 @@ for dir in $archway_proto_dirs; do
   done
 done
 
-neutron_proto_dirs=$(find ./neutron -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $neutron_proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep "option go_package" $file &> /dev/null ; then
-      buf generate --template buf.gen.gogo.yaml $file
-    fi
-  done
-done
-
 cd ..
 
 # move proto files to the right places
