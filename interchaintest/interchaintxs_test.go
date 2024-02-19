5b8c4ceb44ed69851ce2c5891477de59162b0139
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-02-19 15:43:54 +0000

    testing register account

--- interchaintest/interchaintxs_test.go
+++ interchaintest/interchaintxs_test.go
@@ -3,9 +3,11 @@ package interchaintest
 import (
 	"context"
 	"encoding/json"
+	"strconv"
 	"testing"
 
 	"cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	interchaintest "github.com/strangelove-ventures/interchaintest/v7"
 	"github.com/strangelove-ventures/interchaintest/v7/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v7/ibc"
@@ -14,6 +16,7 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v7/testutil"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
+	"gopkg.in/yaml.v2"
 )
 
 func TestInterchainTxs(t *testing.T) {
@@ -21,6 +24,7 @@ func TestInterchainTxs(t *testing.T) {
 		t.Skip("skipping in short mode")
 	}
 
+	numOfVals := 1
 	gaiaChainSpec := &interchaintest.ChainSpec{
 		Name:      "juno",
 		ChainName: "juno",
@@ -28,8 +32,8 @@ func TestInterchainTxs(t *testing.T) {
 		ChainConfig: ibc.ChainConfig{
 			UsingNewGenesisCommand: true,
 		},
+		NumValidators: &numOfVals,
 	}
-	numOfVals := 1
 	archwayChainSpec := GetArchwaySpec("local", numOfVals)
 
 	// Setup the chains
@@ -45,13 +49,13 @@ func TestInterchainTxs(t *testing.T) {
 
 	// Setup the relayer
 	const (
-		path        = "ibc-upgrade-test-path"
+		path        = "interchainx-ica-test-path"
 		relayerName = "relayer"
 	)
 	relayerFactory := interchaintest.NewBuiltinRelayerFactory(
 		ibc.CosmosRly,
 		zaptest.NewLogger(t),
-		relayer.StartupFlags("-b", "100"),
+		relayer.StartupFlags("-b", "100", "-p", "events"),
 	)
 	client, network := interchaintest.DockerSetup(t)
 	relayer := relayerFactory.Build(t, client, network)
@@ -60,7 +64,7 @@ func TestInterchainTxs(t *testing.T) {
 	ic := interchaintest.NewInterchain().
 		AddChain(archwayChain).
 		AddChain(counterpartyChain).
-		AddRelayer(relayer, "relayer").
+		AddRelayer(relayer, relayerName).
 		AddLink(interchaintest.InterchainLink{
 			Chain1:  archwayChain,
 			Chain2:  counterpartyChain,
@@ -76,45 +80,126 @@ func TestInterchainTxs(t *testing.T) {
 		TestName:         t.Name(),
 		Client:           client,
 		NetworkID:        network,
-		SkipPathCreation: false,
+		SkipPathCreation: true,
 	}))
-	t.Cleanup(func() {
-		_ = ic.Close()
-	})
-	err = testutil.WaitForBlocks(ctx, 5, archwayChain, counterpartyChain)
+
+	err = testutil.WaitForBlocks(ctx, 1, archwayChain, counterpartyChain)
 	require.NoError(t, err)
 
-	// Upload the contract to archway chain
 	archwayChainUser := fundChainUser(t, ctx, archwayChain)
 	counterpartyChainUser := fundChainUser(t, ctx, counterpartyChain)
-	codeId, err := archwayChain.StoreContract(ctx, archwayChainUser.KeyName(), "artifacts/contract.wasm")
+
+	// Setting up ibc connections between the two chains
+	err = relayer.GeneratePath(ctx, eRep, archwayChain.Config().ChainID, counterpartyChain.Config().ChainID, path)
 	require.NoError(t, err)
-	require.NotEmpty(t, codeId)
-	// Instantiate the contract
-	contractAddress, err := archwayChain.InstantiateContract(ctx, archwayChainUser.KeyName(), codeId, "{}", false)
+	err = relayer.CreateClients(ctx, eRep, path, ibc.CreateClientOptions{TrustingPeriod: "336h"})
 	require.NoError(t, err)
-	require.NotEmpty(t, contractAddress)
-	// Execute the contract to register an ica account
-	res, err := archwayChain.ExecuteContract(ctx, archwayChainUser.KeyName(), contractAddress, "{}")
+	err = testutil.WaitForBlocks(ctx, 1, archwayChain, counterpartyChain)
 	require.NoError(t, err)
-	require.NotEmpty(t, res)
-	// Wait for a few blocks on both chains so relayer picks up the packet
-	err = testutil.WaitForBlocks(ctx, 5, archwayChain, counterpartyChain)
+	err = relayer.CreateConnections(ctx, eRep, path)
 	require.NoError(t, err)
-	// Check if ica has been registered on the counterparty chain
-	ownerAddress := contractAddress
+	err = testutil.WaitForBlocks(ctx, 1, archwayChain, counterpartyChain)
+	require.NoError(t, err)
 	connections, err := relayer.GetConnections(ctx, eRep, archwayChain.Config().ChainID)
 	require.NoError(t, err)
-	require.Len(t, connections, 1)
-	interchainAccountId := "interchain-account-0"
+	require.GreaterOrEqual(t, len(connections), 1)
+	connection := connections[0]
+
+	err = relayer.StartRelayer(ctx, eRep, path)
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		err := relayer.StopRelayer(ctx, eRep)
+		if err != nil {
+			t.Logf("an error occurred while stopping the relayer: %s", err)
+		}
+		err = ic.Close()
+		if err != nil {
+			t.Logf("an error occurred while closing the interchain: %s", err)
+		}
+	})
+
+	// Upload the contract to archway chain
+	codeId, err := archwayChain.StoreContract(ctx, archwayChainUser.KeyName(), "artifacts/interchaintxs.wasm")
+	require.NoError(t, err)
+	require.NotEmpty(t, codeId)
+
+	// Instantiate the contract
+	initCounter := 1
+	initMsg := "{\"count\":" + strconv.Itoa(initCounter) + ",\"connection_id\":\"" + connection.ID + "\"}"
 	cmd := []string{
-		"archwayd", "q", "interchaintxs", "interchain-account", ownerAddress, connections[0].ID, interchainAccountId,
+		"archwayd", "tx", "wasm", "instantiate", codeId, initMsg,
+		"--label", "interchaintxs", "--admin", archwayChainUser.FormattedAddress(),
+		"--from", archwayChainUser.KeyName(), "--keyring-backend", keyring.BackendTest,
+		"--gas", "auto", "--gas-prices", "0aarch", "--gas-adjustment", "2",
+		"--node", archwayChain.GetRPCAddress(),
+		"--home", archwayChain.HomeDir(),
+		"--chain-id", archwayChain.Config().ChainID,
+		"--output", "json",
+		"-y",
+	}
+	stdout, _, err := archwayChain.Exec(ctx, cmd, nil)
+	require.NoError(t, err)
+	require.NotEmpty(t, stdout)
+
+	err = testutil.WaitForBlocks(ctx, 1, archwayChain)
+	require.NoError(t, err)
+
+	// Getting the contract address
+	cmd = []string{
+		"archwayd", "q", "wasm", "list-contract-by-code", codeId,
+		"--node", archwayChain.GetRPCAddress(),
+		"--home", archwayChain.HomeDir(),
+		"--chain-id", archwayChain.Config().ChainID,
+	}
+	stdout, _, err = archwayChain.Exec(ctx, cmd, nil)
+	require.NoError(t, err, "could not list the contracts")
+	contactsRes := cosmos.QueryContractResponse{}
+	err = yaml.Unmarshal(stdout, &contactsRes)
+	require.NoError(t, err, "could not unmarshal query contract response")
+	contractAddress := contactsRes.Contracts[0]
+
+	// Dump state of the contract
+	var contractRes ContractResponse
+	err = archwayChain.QueryContract(ctx, contractAddress, QueryMsg{DumpState: &struct{}{}}, &contractRes)
+	require.NoError(t, err)
+	require.NotNil(t, contractRes.Data)
+	// Ensure the contract is in the expected state
+	require.Equal(t, int32(initCounter), contractRes.Data.Count)
+	require.Equal(t, archwayChainUser.FormattedAddress(), contractRes.Data.Owner)
+	require.Equal(t, connection.ID, contractRes.Data.ConnectionId)
+	require.Equal(t, "", contractRes.Data.CounterpartyVersion)
+
+	execMsg := `{"register":{}}`
+	cmd = []string{
+		"archwayd", "tx", "wasm", "execute", contractAddress, execMsg,
+		"--from", archwayChainUser.KeyName(), "--keyring-backend", keyring.BackendTest,
+		"--gas", "auto", "--gas-prices", "0aarch", "--gas-adjustment", "2",
+		"--node", archwayChain.GetRPCAddress(),
+		"--home", archwayChain.HomeDir(),
+		"--chain-id", archwayChain.Config().ChainID,
+		"--output", "json",
+		"-y",
+	}
+	stdout, _, err = archwayChain.Exec(ctx, cmd, nil)
+	require.NoError(t, err)
+	require.NotEmpty(t, stdout)
+
+	// Wait for a few blocks on both chains so relayer picks up the packet
+	err = testutil.WaitForBlocks(ctx, 10, archwayChain, counterpartyChain)
+	require.NoError(t, err)
+
+	// Check if ica has been registered on the archway chain
+	ownerAddress := contractAddress
+	interchainAccountId := initCounter
+	cmd = []string{
+		"archwayd", "q", "interchaintxs", "interchain-account", ownerAddress, connection.ID, strconv.Itoa(interchainAccountId),
 		"--node", archwayChain.GetRPCAddress(),
 		"--home", archwayChain.HomeDir(),
 		"--chain-id", archwayChain.Config().ChainID,
 		"--output", "json",
 	}
-	stdout, _, err := archwayChain.Exec(ctx, cmd, nil)
+	stdout, _, err = archwayChain.Exec(ctx, cmd, nil)
 	require.NoError(t, err, "could not query the interchain account")
 	require.NotEmpty(t, stdout)
 
@@ -123,6 +208,16 @@ func TestInterchainTxs(t *testing.T) {
 	require.NoError(t, err, "could not parse the interchain account query response")
 	require.NotEmpty(t, queryRes.InterchainAccountAddress)
 	icaCounterpartyAddress := queryRes.InterchainAccountAddress
+
+	err = archwayChain.QueryContract(ctx, contractAddress, QueryMsg{DumpState: &struct{}{}}, &contractRes)
+	require.NoError(t, err)
+	require.NotNil(t, contractRes.Data)
+	// Ensure the contract is in the expected state
+	require.Equal(t, int32(initCounter), contractRes.Data.Count)
+	require.Equal(t, archwayChainUser.FormattedAddress(), contractRes.Data.Owner)
+	require.Equal(t, connection.ID, contractRes.Data.ConnectionId)
+	require.Equal(t, icaCounterpartyAddress, contractRes.Data.CounterpartyVersion)
+
 	// Check the balance of the ica on the counterparty chain
 	balance, err := counterpartyChain.GetBalance(ctx, icaCounterpartyAddress, counterpartyChain.Config().Denom)
 	require.NoError(t, err)
@@ -138,19 +233,57 @@ func TestInterchainTxs(t *testing.T) {
 	balance, err = counterpartyChain.GetBalance(ctx, icaCounterpartyAddress, counterpartyChain.Config().Denom)
 	require.NoError(t, err)
 	require.Equal(t, int64(1000), balance.Int64())
-	// SubmitTx on contract which will send money back to the initial account
-	res, err = archwayChain.ExecuteContract(ctx, archwayChainUser.KeyName(), contractAddress, "{}")
+
+	// SubmitTx on contract which will send these tokens back to the og address
+	res, err := archwayChain.ExecuteContract(ctx, archwayChainUser.KeyName(), contractAddress, "{}")
 	require.NoError(t, err)
 	require.NotEmpty(t, res)
-	// Wait for a while to ensure the relayer picks up the packet
-	err = testutil.WaitForBlocks(ctx, 5, archwayChain, counterpartyChain)
-	require.NoError(t, err)
-	// Check the balance of the ica account on counterparty chain. Should be none
-	balance, err = counterpartyChain.GetBalance(ctx, icaCounterpartyAddress, counterpartyChain.Config().Denom)
-	require.NoError(t, err)
-	require.Equal(t, int64(0), balance.Int64())
+	// // Wait for a while to ensure the relayer picks up the packet
+	// err = testutil.WaitForBlocks(ctx, 5, archwayChain, counterpartyChain)
+	// require.NoError(t, err)
+	// // Check the balance of the ica account on counterparty chain. Should be none
+	// balance, err = counterpartyChain.GetBalance(ctx, icaCounterpartyAddress, counterpartyChain.Config().Denom)
+	// require.NoError(t, err)
+	// require.Equal(t, int64(0), balance.Int64())
+
+	// // Wait for a few blocks on both chains so relayer picks up the packet
+	// err = testutil.WaitForBlocks(ctx, 10, archwayChain, counterpartyChain)
+	// require.NoError(t, err)
+
+	// h, err := archwayChain.Height(ctx)
+	// require.NoError(t, err)
+	// err = archwayChain.StopAllNodes(ctx)
+	// require.NoError(t, err)
+	// state, err := archwayChain.ExportState(ctx, int64(h))
+	// require.NoError(t, err)
+	// err = ioutil.WriteFile("./testdata/archway_state.json", []byte(state), 0644)
+	// require.NoError(t, err)
+
+	// h, err = counterpartyChain.Height(ctx)
+	// require.NoError(t, err)
+	// err = counterpartyChain.StopAllNodes(ctx)
+	// require.NoError(t, err)
+	// state, err = counterpartyChain.ExportState(ctx, int64(h))
+	// require.NoError(t, err)
+	// err = ioutil.WriteFile("./testdata/juno_state.json", []byte(state), 0644)
+	// require.NoError(t, err)
 }
 
 type InterchainAccountAccountQueryResponse struct {
 	InterchainAccountAddress string `json:"interchain_account_address"`
+}
+
+type ContractResponse struct {
+	Data ContractResponseObj `json:"data"`
+}
+
+type ContractResponseObj struct {
+	Count               int32  `json:"count"`
+	Owner               string `json:"owner"`
+	ConnectionId        string `json:"connection_id"`
+	CounterpartyVersion string `json:"counterparty_version"`
+}
+
+type QueryMsg struct {
+	DumpState *struct{} `json:"dump_state"`
 }
