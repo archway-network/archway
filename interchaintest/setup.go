70b00b4db186b1ba618018640800d752324a090d
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-02-26 18:20:22 +0000

    fixing cwica interchaintest

--- interchaintest/setup.go
+++ interchaintest/setup.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	initialVersion = "v6.0.1 " // The last release of the chain. The one the mainnet is running on
-	upgradeName    = "latest"  // The next upgrade name. Should match the upgrade handler.
+	initialVersion = "v6.0.1" // The last release of the chain. The one the mainnet is running on
+	upgradeName    = "latest" // The next upgrade name. Should match the upgrade handler.
 	chainName      = "archway"
 )
 
