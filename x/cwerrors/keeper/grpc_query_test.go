4d142879375b84410ac6ccc87ec6f39d113fd3f9
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-03-07 10:19:05 +0000

    adding grpc query test

--- x/cwerrors/keeper/grpc_query_test.go
+++ x/cwerrors/keeper/grpc_query_test.go
@@ -1,1 +1,99 @@
 package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	e2eTesting "github.com/archway-network/archway/e2e/testing"
+	"github.com/archway-network/archway/pkg/testutils"
+	cwerrorsKeeper "github.com/archway-network/archway/x/cwerrors/keeper"
+	"github.com/archway-network/archway/x/cwerrors/types"
+)
+
+func (s *KeeperTestSuite) TestErrors() {
+	ctx, keeper := s.chain.GetContext(), s.chain.GetApp().Keepers.CWErrorsKeeper
+	contractViewer := testutils.NewMockContractViewer()
+	keeper.SetWasmKeeper(contractViewer)
+	contractAddresses := e2eTesting.GenContractAddresses(3)
+	contractAddr := contractAddresses[0]
+	contractAddr2 := contractAddresses[1]
+	contractAdminAcc := s.chain.GetAccount(0)
+	contractViewer.AddContractAdmin(
+		contractAddr.String(),
+		contractAdminAcc.Address.String(),
+	)
+	contractViewer.AddContractAdmin(
+		contractAddr2.String(),
+		contractAdminAcc.Address.String(),
+	)
+
+	queryServer := cwerrorsKeeper.NewQueryServer(keeper)
+
+	// Sending nil query
+	_, err := queryServer.Errors(sdk.WrapSDKContext(ctx), nil)
+	s.Require().Error(err)
+
+	// Set errors for block 1
+	// 2 errors for contract1
+	// 1 error for contract2
+	contract1Err := types.SudoError{
+		ContractAddress: contractAddr.String(),
+		ModuleName:      "test",
+	}
+	contract2Err := types.SudoError{
+		ContractAddress: contractAddr2.String(),
+		ModuleName:      "test",
+	}
+	err = keeper.SetError(ctx, contract1Err)
+	s.Require().NoError(err)
+	err = keeper.SetError(ctx, contract1Err)
+	s.Require().NoError(err)
+	err = keeper.SetError(ctx, contract2Err)
+	s.Require().NoError(err)
+
+	// Check number of errors match
+	res, err := queryServer.Errors(sdk.WrapSDKContext(ctx), &types.QueryErrorsRequest{ContractAddress: contractAddr.String()})
+	s.Require().NoError(err)
+	s.Require().Len(res.Errors, 2)
+	res, err = queryServer.Errors(sdk.WrapSDKContext(ctx), &types.QueryErrorsRequest{ContractAddress: contractAddr2.String()})
+	s.Require().NoError(err)
+	s.Require().Len(res.Errors, 1)
+
+	// Increment block height
+	ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
+
+	// Set errors for block 2
+	// 1 error for contract1
+	// 1 error for contract2
+	err = keeper.SetError(ctx, contract1Err)
+	s.Require().NoError(err)
+	err = keeper.SetError(ctx, contract2Err)
+	s.Require().NoError(err)
+
+	// Check number of errors match
+	res, err = queryServer.Errors(sdk.WrapSDKContext(ctx), &types.QueryErrorsRequest{ContractAddress: contractAddr.String()})
+	s.Require().NoError(err)
+	s.Require().Len(res.Errors, 3)
+	res, err = queryServer.Errors(sdk.WrapSDKContext(ctx), &types.QueryErrorsRequest{ContractAddress: contractAddr2.String()})
+	s.Require().NoError(err)
+	s.Require().Len(res.Errors, 2)
+}
+
+func (s *KeeperTestSuite) TestParams() {
+	ctx, keeper := s.chain.GetContext(), s.chain.GetApp().Keepers.CWErrorsKeeper
+	queryServer := cwerrorsKeeper.NewQueryServer(keeper)
+
+	// Sending nil query
+	_, err := queryServer.Params(sdk.WrapSDKContext(ctx), nil)
+	s.Require().Error(err)
+
+	// Set params
+	params := types.Params{
+		ErrorStoredTime: 100,
+	}
+	keeper.SetParams(ctx, params)
+
+	// Query params
+	res, err := queryServer.Params(sdk.WrapSDKContext(ctx), &types.QueryParamsRequest{})
+	s.Require().NoError(err)
+	s.Require().Equal(params, res.Params)
+}
