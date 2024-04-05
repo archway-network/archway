a0235316098bb3cd5dcb1232314b08664d851d4f
Spoorthi <9302666+spoo-bar@users.noreply.github.com>
2024-04-05 08:33:57 +0530

    fix(x/callback): error code was identified incorrectly (#552)
    * adding new error type
    
    * refactor the callback abci tests
    
    * checking against module error codes
    
    * regenerating proto files
    
    * linting 🧹

--- x/callback/abci_test.go
+++ x/callback/abci_test.go
@@ -11,9 +11,9 @@ import (
 	wasmdTypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
 	e2eTesting "github.com/archway-network/archway/e2e/testing"
-	callbackabci "github.com/archway-network/archway/x/callback"
 	callbackKeeper "github.com/archway-network/archway/x/callback/keeper"
 	"github.com/archway-network/archway/x/callback/types"
+	cwerrortypes "github.com/archway-network/archway/x/cwerrors/types"
 )
 
 const (
@@ -25,7 +25,8 @@ const (
 
 func TestEndBlocker(t *testing.T) {
 	chain := e2eTesting.NewTestChain(t, 1)
-	ctx, keeper := chain.GetContext(), chain.GetApp().Keepers.CallbackKeeper
+	keeper := chain.GetApp().Keepers.CallbackKeeper
+	errorsKeeper := chain.GetApp().Keepers.CWErrorsKeeper
 	msgServer := callbackKeeper.NewMsgServer(keeper)
 	contractAdminAcc := chain.GetAccount(0)
 
@@ -38,13 +39,7 @@ func TestEndBlocker(t *testing.T) {
 	codeID := chain.UploadContract(contractAdminAcc, "../../contracts/callback-test/artifacts/callback_test.wasm", wasmdTypes.DefaultUploadAccess)
 	initMsg := CallbackContractInstantiateMsg{Count: 100}
 	contractAddr, _ := chain.InstantiateContract(contractAdminAcc, codeID, contractAdminAcc.Address.String(), "callback_test", nil, initMsg)
-
-	// Calculating the callback fees for the next height
-	currentBlockHeight := ctx.BlockHeight()
-	callbackHeight := currentBlockHeight + 1
-	futureResFee, blockResFee, txFee, err := keeper.EstimateCallbackFees(ctx, callbackHeight)
-	require.NoError(t, err)
-	feesToPay := futureResFee.Add(blockResFee).Add(txFee)
+	chain.NextBlock(1)
 
 	testCases := []struct {
 		testCase      string
@@ -74,6 +69,10 @@ func TestEndBlocker(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("Case: %s", tc.testCase), func(t *testing.T) {
+			ctx := chain.GetContext()
+			feesToPay, err := getCallbackRegistrationFees(chain)
+			require.NoError(t, err)
+
 			reqMsg := &types.MsgRequestCallback{
 				ContractAddress: contractAddr.String(),
 				JobId:           tc.jobId,
@@ -81,76 +80,109 @@ func TestEndBlocker(t *testing.T) {
 				Sender:          contractAdminAcc.Address.String(),
 				Fees:            feesToPay,
 			}
-			_, err := msgServer.RequestCallback(sdk.WrapSDKContext(ctx), reqMsg)
+			_, err = msgServer.RequestCallback(sdk.WrapSDKContext(ctx), reqMsg)
 			require.NoError(t, err)
 
-			// Increment block height and run end blocker
-			ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
-			require.Equal(t, ctx.BlockHeight(), reqMsg.CallbackHeight)
-			_ = callbackabci.EndBlocker(ctx, keeper, chain.GetApp().Keepers.WASMKeeper, chain.GetApp().Keepers.CWErrorsKeeper)
+			// Increment block height and run end blocker at the next block
+			chain.NextBlock(1)
+			chain.NextBlock(1)
 
 			// Checking if the count value is as expected
-			count := getCount(t, chain, ctx, contractAddr)
+			count := getCount(t, chain, contractAddr)
 			require.Equal(t, tc.expectedCount, count)
 		})
 	}
 
-	params, err := keeper.GetParams(ctx)
+	// Ensure error is captured by the cwerrors module - the case is when job id = 2
+	sudoErrs, err := errorsKeeper.GetErrorsByContractAddress(chain.GetContext(), contractAddr)
+	require.NoError(t, err)
+	require.Len(t, sudoErrs, 1)
+	require.Equal(t, "SomeError: execute wasm contract failed", sudoErrs[0].ErrorMessage)
+	require.Equal(t, "callback", sudoErrs[0].ModuleName)
+	require.Equal(t, int32(types.ModuleErrors_ERR_CONTRACT_EXECUTION_FAILED), sudoErrs[0].ErrorCode)
+
+	// Registering the contract for error subscription
+	_, err = errorsKeeper.SetSubscription(chain.GetContext(), contractAddr, contractAddr, sdk.NewInt64Coin(sdk.DefaultBondDenom, 0))
+	require.NoError(t, err)
+
+	params, err := keeper.GetParams(chain.GetContext())
 	require.NoError(t, err)
 
 	// TEST CASE: Test CallbackGasLimit limit value reduced
 	// First we set the params value to default
 	// Register a callback for next block
+	feesToPay, err := getCallbackRegistrationFees(chain)
+	require.NoError(t, err)
 	reqMsg := &types.MsgRequestCallback{
 		ContractAddress: contractAddr.String(),
 		JobId:           INCREMENT_JOBID,
-		CallbackHeight:  ctx.BlockHeight() + 1,
+		CallbackHeight:  chain.GetContext().BlockHeight() + 1,
 		Sender:          contractAdminAcc.Address.String(),
 		Fees:            feesToPay,
 	}
-	_, err = msgServer.RequestCallback(sdk.WrapSDKContext(ctx), reqMsg)
+	_, err = msgServer.RequestCallback(sdk.WrapSDKContext(chain.GetContext()), reqMsg)
 	require.NoError(t, err)
 
 	// Setting the callbackGasLimit param to 1
 	params.CallbackGasLimit = 1
-	err = keeper.SetParams(ctx, params)
+	err = keeper.SetParams(chain.GetContext(), params)
 	require.NoError(t, err)
 
 	// Increment block height and run end blocker
-	ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
-	_ = callbackabci.EndBlocker(ctx, keeper, chain.GetApp().Keepers.WASMKeeper, chain.GetApp().Keepers.CWErrorsKeeper)
+	chain.NextBlock(1)
+	chain.NextBlock(1)
 
 	// Checking if the count value has incremented.
 	// Should have incremented as the callback should have access to higher gas limit as it was registered before the gas limit was reduced
-	count := getCount(t, chain, ctx, contractAddr)
+	count := getCount(t, chain, contractAddr)
 	require.Equal(t, initMsg.Count+1, count)
 
 	// TEST CASE: OUT OF GAS ERROR
 	// Reserving a callback for next block
 	// This callback should fail as it consumes more gas than allowed
+	feesToPay, err = getCallbackRegistrationFees(chain)
+	require.NoError(t, err)
 	reqMsg = &types.MsgRequestCallback{
 		ContractAddress: contractAddr.String(),
 		JobId:           INCREMENT_JOBID,
-		CallbackHeight:  ctx.BlockHeight() + 1,
+		CallbackHeight:  chain.GetContext().BlockHeight() + 1,
 		Sender:          contractAdminAcc.Address.String(),
 		Fees:            feesToPay,
 	}
-	_, err = msgServer.RequestCallback(sdk.WrapSDKContext(ctx), reqMsg)
+	_, err = msgServer.RequestCallback(sdk.WrapSDKContext(chain.GetContext()), reqMsg)
 	require.NoError(t, err)
 
 	// Increment block height and run end blocker
-	ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
-	_ = callbackabci.EndBlocker(ctx, keeper, chain.GetApp().Keepers.WASMKeeper, chain.GetApp().Keepers.CWErrorsKeeper)
+	chain.NextBlock(1)
+	chain.NextBlock(1)
 
 	// Checking if the count value has incremented. Should not have incremented as the callback failed due to out of gas error
-	count = getCount(t, chain, ctx, contractAddr)
+	count = getCount(t, chain, contractAddr)
 	require.Equal(t, initMsg.Count+1, count)
+
+	sudoErrs, err = errorsKeeper.GetErrorsByContractAddress(chain.GetContext(), contractAddr)
+	require.NoError(t, err)
+	require.Len(t, sudoErrs, 2)
+	require.Equal(t, "cwerrors", sudoErrs[1].ModuleName) // because Sudo::Error entrypoint does not exist on the contract
+	require.Equal(t, int32(cwerrortypes.ModuleErrors_ERR_CALLBACK_EXECUTION_FAILED), sudoErrs[1].ErrorCode)
+}
+
+func getCallbackRegistrationFees(chain *e2eTesting.TestChain) (sdk.Coin, error) {
+	ctx := chain.GetContext()
+	currentBlockHeight := ctx.BlockHeight()
+	callbackHeight := currentBlockHeight + 1
+	futureResFee, blockResFee, txFee, err := chain.GetApp().Keepers.CallbackKeeper.EstimateCallbackFees(ctx, callbackHeight)
+	if err != nil {
+		return sdk.Coin{}, err
+	}
+	feesToPay := futureResFee.Add(blockResFee).Add(txFee)
+	return feesToPay, nil
 }
 
 // getCount is a helper function to get the contract's count value
-func getCount(t *testing.T, chain *e2eTesting.TestChain, ctx sdk.Context, contractAddr sdk.AccAddress) int32 {
+func getCount(t *testing.T, chain *e2eTesting.TestChain, contractAddr sdk.AccAddress) int32 {
 	getCountQuery := "{\"get_count\":{}}"
-	resp, err := chain.GetApp().Keepers.WASMKeeper.QuerySmart(ctx, contractAddr, []byte(getCountQuery))
+	resp, err := chain.GetApp().Keepers.WASMKeeper.QuerySmart(chain.GetContext(), contractAddr, []byte(getCountQuery))
 	require.NoError(t, err)
 	var getCountResp CallbackContractQueryMsg
 	err = json.Unmarshal(resp, &getCountResp)
