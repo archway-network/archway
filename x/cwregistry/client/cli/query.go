743fc9fbdc1fe3141dc727815bb6d6b5820aae1c
Spoorthi <9302666+spoo-bar@users.noreply.github.com>
2024-07-01 13:39:29 +0100

    adding cli

--- x/cwregistry/client/cli/query.go
+++ x/cwregistry/client/cli/query.go
@@ -1,9 +1,12 @@
 package cli
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 
 	"github.com/archway-network/archway/x/cwregistry/types"
@@ -18,6 +21,129 @@ func GetQueryCmd(_ string) *cobra.Command {
 		SuggestionsMinimumDistance: 1,
 		RunE:                       client.ValidateCmd,
 	}
+
+	cmd.AddCommand(CmdQueryCodeMetadata())
+	cmd.AddCommand(CmdQueryContractMetadata())
+	cmd.AddCommand(CmdQueryCodeSchema())
+	cmd.AddCommand(CmdQueryContractSchema())
+
+	return cmd
+}
+
+func CmdQueryCodeMetadata() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "code-metadata",
+		Short: "",
+		Long:  "",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			codeID, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+			res, err := queryClient.CodeMetadata(context.Background(), &types.QueryCodeMetadataRequest{
+				CodeId: codeID,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryContractMetadata() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "contract-metadata",
+		Short: "",
+		Long:  "",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			contractAddr := args[0]
+			res, err := queryClient.ContractMetadata(context.Background(), &types.QueryContractMetadataRequest{
+				ContractAddress: contractAddr,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryCodeSchema() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "code-schema",
+		Short: "",
+		Long:  "",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			codeID, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+			res, err := queryClient.CodeSchema(context.Background(), &types.QueryCodeSchemaRequest{
+				CodeId: codeID,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdQueryContractSchema() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "contract-schema",
+		Short: "",
+		Long:  "",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			contractAddr := args[0]
+			res, err := queryClient.ContractSchema(context.Background(), &types.QueryContractSchemaRequest{
+				ContractAddress: contractAddr,
+			})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
 
 	return cmd
 }
--- x/cwregistry/client/cli/tx.go
+++ x/cwregistry/client/cli/tx.go
@@ -1,9 +1,14 @@
 package cli
 
 import (
+	"strconv"
+
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cobra"
 
+	"github.com/archway-network/archway/pkg"
 	"github.com/archway-network/archway/x/cwregistry/types"
 )
 
@@ -16,6 +21,89 @@ func GetTxCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+
+	cmd.AddCommand(CmdRegisterCode())
+	cmd.AddCommand(CmdRegisterContract())
+
+	return cmd
+}
+
+func CmdRegisterContract() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "register-contract [contract-address]",
+		Args:  cobra.ExactArgs(1),
+		Short: "",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			senderAddr := clientCtx.GetFromAddress()
+			contractAddr := args[0]
+
+			sourceMetadata := parseSourceMetadata(cmd)
+			sourceBuilder := parseSourceBuilder(cmd)
+			schema := parseSchema(cmd)
+			contacts, err := pkg.GetStringSliceFlag(cmd, flagContacts, true)
+			if err != nil {
+				return err
+			}
+			msg := types.MsgRegisterContract{
+				Sender:          senderAddr.String(),
+				ContractAddress: contractAddr,
+				SourceMetadata:  &sourceMetadata,
+				SourceBuilder:   &sourceBuilder,
+				Schema:          schema,
+				Contacts:        contacts,
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	addFlags(cmd)
+
+	return cmd
+}
+
+func CmdRegisterCode() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "register-code [code-id]",
+		Args:  cobra.ExactArgs(1),
+		Short: "",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			senderAddr := clientCtx.GetFromAddress()
+			codeID, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+			sourceMetadata := parseSourceMetadata(cmd)
+			sourceBuilder := parseSourceBuilder(cmd)
+			schema := parseSchema(cmd)
+			contacts, err := pkg.GetStringSliceFlag(cmd, flagContacts, true)
+			if err != nil {
+				return err
+			}
+			msg := types.MsgRegisterCode{
+				Sender:         senderAddr.String(),
+				CodeId:         codeID,
+				SourceMetadata: &sourceMetadata,
+				SourceBuilder:  &sourceBuilder,
+				Schema:         schema,
+				Contacts:       contacts,
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+	addFlags(cmd)
 
 	return cmd
 }
