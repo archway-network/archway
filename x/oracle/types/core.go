9f757b0c6dbff3ff60e0a41293c68ae095f27075
SpekalsG3 <karpick.kirill@gmail.com>
2024-11-07 20:17:30 +0300

    fix(oracle): IsPeriodLastBlock

--- x/oracle/types/core.go
+++ x/oracle/types/core.go
@@ -4,5 +4,5 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 
 // IsPeriodLastBlock returns true if we are at the last block of the period
 func IsPeriodLastBlock(ctx sdk.Context, blocksPerPeriod uint64) bool {
-	return ((uint64)(ctx.BlockHeight())+1)%blocksPerPeriod == 0
+	return ((uint64)(ctx.BlockHeight()))%blocksPerPeriod == 0
 }
